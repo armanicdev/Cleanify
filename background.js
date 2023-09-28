be66e1996e35fc442b8073f019d64cb91c40289d
Armanic <96264464+armanicdev@users.noreply.github.com>
2023-09-28 16:04:06 +0300

    Comments

--- background.js
+++ background.js
@@ -1,12 +1,17 @@
+// Add a listener for messages from the extension
 chrome.runtime.onMessage.addListener((message, sender) => {
+  // Check if the message action is either 'startUnsubscribe' or 'stopUnsubscribe'
   if (message.action === 'startUnsubscribe' || message.action === 'stopUnsubscribe') {
+    // Check if the sender has a tab and a URL that starts with 'http'
     if (sender.tab && sender.tab.url && sender.tab.url.startsWith('http')) {
+      // Execute a content script in the sender's tab
       chrome.scripting.executeScript({
         target: { tabId: sender.tab.id },
         function: (message) => {
+          // Send the message to the content script
           chrome.runtime.sendMessage(message);
         },
-        args: [message],
+        args: [message], // Pass the original message to the content script
       });
     }
   }
--- popup.js
+++ popup.js
@@ -1,21 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
+  // Get references to HTML elements
   const startButton = document.getElementById('startButton');
   const stopButton = document.getElementById('stopButton');
   const messageElement = document.getElementById('message');
 
+  // Function to set the text of the message element
   function setMessageText(text) {
     messageElement.textContent = text;
   }
 
+  // Function to enable/disable buttons
   function toggleButtons(startEnabled, stopEnabled) {
     startButton.disabled = !startEnabled;
     stopButton.disabled = !stopEnabled;
   }
 
+  // Check if the current tab is the YouTube manage page
   function isYouTubeManagePage(url) {
     return url.startsWith('https://www.youtube.com/') && url.includes('/feed/channels');
   }
 
+  // Check the current tab on extension load
   function checkYouTubeTab() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       const activeTab = tabs[0];
@@ -29,10 +34,12 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
+  // Event listener for the start button
   startButton.addEventListener('click', function() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       const activeTab = tabs[0];
       if (isYouTubeManagePage(activeTab.url)) {
+        // Send a message to the content script to start unsubscribing
         chrome.tabs.sendMessage(activeTab.id, { action: 'startUnsubscribe' });
         toggleButtons(false, true);
       } else {
@@ -41,13 +48,16 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
+  // Event listener for the stop button
   stopButton.addEventListener('click', function() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       const activeTab = tabs[0];
+      // Send a message to the content script to stop unsubscribing
       chrome.tabs.sendMessage(activeTab.id, { action: 'stopUnsubscribe' });
       toggleButtons(true, false);
     });
   });
 
+  // Check the current tab when the extension loads
   checkYouTubeTab();
 });
