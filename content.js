b8097c043549bf8f2a357072618888ab84b31ffd
Armanic <96264464+armanicdev@users.noreply.github.com>
2023-10-03 00:53:58 +0300

    Animation & Optimization

--- content.js
+++ content.js
@@ -1,5 +1,3 @@
-// Credit: ArmanicYT
-
 // Define the delay time for unsubscribing (in milliseconds)
 const UNSUBSCRIBE_DELAY_TIME = 500;
 
@@ -9,30 +7,23 @@ let isUnsubscribing = false;
 // Function to unsubscribe from a list of channels
 async function unsubscribeFromChannels(channels) {
   for (let i = 0; i < channels.length; i++) {
-    // Check if unsubscribing has been stopped
     if (!isUnsubscribing) {
       console.log("Unsubscribe process stopped.");
       break;
     }
 
-    // Get the current channel element
     const channel = channels[i];
 
-    // Find the "Unsubscribe" button for the channel
     const unsubscribeButton = channel.querySelector('[aria-label^="Unsubscribe from"]');
 
     if (unsubscribeButton) {
-      // Click the "Unsubscribe" button
       unsubscribeButton.click();
 
-      // Wait for a short delay
       await new Promise((resolve) => setTimeout(resolve, UNSUBSCRIBE_DELAY_TIME));
 
-      // Find the confirmation button for unsubscribing
       const confirmButton = document.querySelector('yt-confirm-dialog-renderer [aria-label^="Unsubscribe"]');
 
       if (confirmButton) {
-        // Click the confirmation button
         confirmButton.click();
         console.log(`Unsubscribed ${i + 1}/${channels.length}`);
       }
@@ -42,18 +33,14 @@ async function unsubscribeFromChannels(channels) {
 
 // Add a listener for the page load event
 window.addEventListener('load', async () => {
-  // Find all channel elements on the page
   const channels = Array.from(document.querySelectorAll('ytd-channel-renderer'));
   console.log(`${channels.length} channels found.`);
 
-  // Listen for messages from the extension
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    // Start the unsubscribe process if requested and not already in progress
     if (message.action === 'startUnsubscribe' && !isUnsubscribing) {
       isUnsubscribing = true;
       unsubscribeFromChannels(channels);
     } else if (message.action === 'stopUnsubscribe') {
-      // Stop the unsubscribe process
       isUnsubscribing = false;
     }
   });
--- popup.js
+++ popup.js
@@ -1,20 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
+// Function to display a specific page by its ID
+function showPage(pageId) {
   const pages = document.querySelectorAll('.page');
+  pages.forEach((page) => {
+    page.style.display = 'none';
+  });
+
+  const pageToShow = document.getElementById(pageId);
+  pageToShow.style.display = 'block';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Select elements from the DOM
   const gridItems = document.querySelectorAll('.grid-item'); // Add back grid items
   const backButton = document.getElementById('backButton');
   const startButton = document.getElementById('startButton');
   const stopButton = document.getElementById('stopButton');
   const messageElement = document.getElementById('message');
 
-  function showPage(pageId) {
-    pages.forEach((page) => {
-      page.style.display = 'none';
-    });
-
-    const pageToShow = document.getElementById(pageId);
-    pageToShow.style.display = 'block';
-  }
-
+  // Add click event listeners to grid items
   gridItems.forEach((item, index) => {
     item.addEventListener('click', () => {
       if (index === 0) {
@@ -24,11 +27,13 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
+  // Add click event listener to the back button
   backButton.addEventListener('click', () => {
     showPage('defaultPage');
     chrome.storage.local.set({ activePage: 'defaultPage' });
   });
 
+  // Function to reset the extension's active page
   function resetExtension() {
     chrome.storage.local.get('activePage', function (result) {
       const activePage = result.activePage || 'defaultPage';
@@ -36,25 +41,31 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
+  // Add a beforeunload event listener to reset the extension
   window.addEventListener('beforeunload', () => {
     resetExtension();
   });
 
+  // Initialize the extension page
   resetExtension();
 
+  // Function to set the message text
   function setMessageText(text) {
     messageElement.textContent = text;
   }
 
+  // Function to toggle button states
   function toggleButtons(startEnabled, stopEnabled) {
     startButton.disabled = !startEnabled;
     stopButton.disabled = !stopEnabled;
   }
 
+  // Function to check if a URL is a YouTube Manage page
   function isYouTubeManagePage(url) {
     return url.startsWith('https://www.youtube.com/') && url.includes('/feed/channels');
   }
 
+  // Add click event listener to the start button
   startButton.addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       const activeTab = tabs[0];
@@ -68,6 +79,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
+  // Add click event listener to the stop button
   stopButton.addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       const activeTab = tabs[0];
