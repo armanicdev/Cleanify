c65ab976647c773d0ffce9d3a3ce6bc01ee2abf1
Armanic <96264464+armanicdev@users.noreply.github.com>
2023-10-12 13:27:28 +0300

    Animations

--- popup.js
+++ popup.js
@@ -1,17 +1,20 @@
+// Function to show a specific page and hide others
 function showPage(pageId) {
   const pages = document.querySelectorAll('.page');
   const pageToShow = document.getElementById(pageId);
 
   if (!pageToShow) {
-    return;
+    return; // Return if the specified page is not found
   }
 
+  // Hide all pages
   pages.forEach((page) => {
     page.style.opacity = '0';
     page.style.pointerEvents = 'none';
     page.style.position = 'absolute';
   });
 
+  // Show the specified page
   pageToShow.style.display = 'block';
   pageToShow.style.opacity = '1';
   pageToShow.style.pointerEvents = 'auto';
@@ -19,26 +22,31 @@ function showPage(pageId) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+  // DOM elements
   const startButton = document.getElementById('startButton');
   const stopButton = document.getElementById('stopButton');
   const messageElement = document.getElementById('message');
   const backButton = document.getElementById('backButton');
   const gridItems = document.querySelectorAll('.grid-item');
 
+  // Function to set message text
   const setMessageText = (text) => {
     messageElement.textContent = text;
   };
 
+  // Function to toggle start and stop buttons based on their enable status
   const toggleButtons = (startEnabled, stopEnabled) => {
     console.log('Toggle buttons called with:', startEnabled, stopEnabled);
     startButton.classList.toggle('disabled', !startEnabled);
     stopButton.classList.toggle('disabled', !stopEnabled);
   };
 
+  // Function to check if the URL is a YouTube feed tab
   function isYouTubeFeedTab(url) {
     return url.startsWith('https://www.youtube.com/feed/channels');
   }
 
+  // Click event listeners for grid items
   gridItems.forEach((item, index) => {
     item.addEventListener('click', () => {
       if (index === 0) {
@@ -48,18 +56,22 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
+  // Click event listener for the back button
   backButton.addEventListener('click', () => {
     showPage('defaultPage');
     chrome.storage.local.set({ activePage: 'defaultPage' });
   });
 
+  // Event listener for the window unload event
   window.addEventListener('beforeunload', resetExtension);
   resetExtension();
 
+  // Event listener for the tab activation event
   chrome.tabs.onActivated.addListener(({ tabId }) => {
     checkYouTubeTab(tabId);
   });
 
+  // Click event listener for the start button
   startButton.addEventListener('click', () => {
     chrome.tabs.query({ active: true, currentWindow: true }, ([activeTab]) => {
       if (isYouTubeFeedTab(activeTab?.url)) {
@@ -72,6 +84,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
+  // Click event listener for the stop button
   stopButton.addEventListener('click', () => {
     chrome.tabs.query({ active: true, currentWindow: true }, ([activeTab]) => {
       if (!stopButton.classList.contains('disabled')) {
@@ -82,6 +95,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
+  // Get stored button enable status and update button states
   chrome.storage.local.get(['startButtonEnabled', 'stopButtonEnabled'], ({ startButtonEnabled, stopButtonEnabled }) => {
     chrome.tabs.query({ active: true, currentWindow: true }, ([activeTab]) => {
       const url = activeTab?.url || '';
@@ -96,6 +110,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+// Function to reset the extension state
 function resetExtension() {
   chrome.storage.local.get(['activePage', 'startButtonEnabled', 'stopButtonEnabled'], ({ activePage, startButtonEnabled, stopButtonEnabled }) => {
     const pageToShow = activePage || 'defaultPage';
@@ -104,11 +119,13 @@ function resetExtension() {
   });
 }
 
+// Function to check if the given tab is a YouTube feed tab
 function checkYouTubeTab(tabId) {
   chrome.tabs.get(tabId, (tab) => {
     if (tab) {
       const url = tab.url || '';
       if (isYouTubeFeedTab(url)) {
+        // Additional logic for YouTube feed tab if needed
       }
     }
   });
